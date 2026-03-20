0e788ef5fe0b589fd21cae1a3f3d1fa2606f58e2
komandor <koletttut@gmail.com>
2026-03-20 16:30:33 +0200

    fix11

---
+++ src/task-6.ts
@@ -0,0 +1,7 @@
+function getFirstElement<T>(arr: T[]) :T {
+  return arr[0];
+}
+
+console.log(getFirstElement([1, 2, 3]));
+console.log(getFirstElement(["a", "b", "c"]));    
+console.log(getFirstElement([true, false, true]));
